f2ae3d0367d8428eda5696191725cf423d5548fd
John Peterson <jwpeterson@gmail.com>
2020-07-17 10:28:22 -0500

    Comment out currently non-working lines to get reduced_basis_ex4 compiling.

--- examples/reduced_basis/reduced_basis_ex4/reduced_basis_ex4.C
+++ examples/reduced_basis/reduced_basis_ex4/reduced_basis_ex4.C
@@ -176,8 +176,9 @@ int main (int argc, char ** argv)
       // Write out the basis functions, if requested
       if (store_basis_functions)
         {
-          eim_construction.get_rb_evaluation().write_out_basis_functions(eim_construction.get_explicit_system(),
-                                                                         "eim_data");
+          // FIXME: This needs to be implemented in the new version of RBEIMEvaluation
+          // eim_construction.get_rb_evaluation().write_out_basis_functions(eim_construction.get_explicit_system(),
+          //                                                                "eim_data");
 
           rb_construction.get_rb_evaluation().write_out_basis_functions(rb_construction,
                                                                         "rb_data");
@@ -214,7 +215,10 @@ int main (int argc, char ** argv)
       if (store_basis_functions)
         {
           // read in the data from files
-          eim_rb_eval.read_in_basis_functions(eim_construction.get_explicit_system(), "eim_data");
+
+          // FIXME: This needs to be implemented in the new version of RBEIMEvaluation
+          // eim_rb_eval.read_in_basis_functions(eim_construction.get_explicit_system(), "eim_data");
+
           rb_eval.read_in_basis_functions(rb_construction, "rb_data");
 
           rb_construction.load_rb_solution();
